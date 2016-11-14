13162683a941dc2c73305cfcce66a3c9e287976c
Amal Abukar <amalabukar@amals-mbp.apollosrc.root>
2016-11-14 15:29:05 -0600

    first assignment

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.carType = "Ford Galaxy";
+        c.color = "black";
+        c.modelYear =  2001;
+        c.isFirstCar = false;
+
+
+        Spotify s= new Spotify();
+        s.username = "Amal";
+        s.age= 25;
+        s.yearActivated= 2012;
+        s.premiumUser= false;
+
+        Coffee cf = new Coffee();
+        cf.type="latte";
+        cf.cafeChoice= "Starbucks";
+        cf.sugar= 2;
+        cf.addsMilk= true;
+
+        Exercise e= new Exercise();
+        e.attendsGym="Yes";
+        e.gymVisits= "3 times per week";
+        e.diets= true;
+        e.jogs= false;
+
+        DC d=new DC();
+        d.population= "650,000";
+        d.precipation="3 inches of snow";
+        d.sportsTeams=10;
+        d.universities=8;
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
